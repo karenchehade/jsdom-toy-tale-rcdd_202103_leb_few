04c95714d4d06334b3d3ab26d78909280c092aa0
karen chehade <karen_chehade@hotmail.com>
2021-05-03 18:41:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,9 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const toyFormContainer = document.querySelector(".container");
   const toy-collection = document.getElementById('toy-collection');
   const form = document.querySelector(".add-toy-form");
+  const option ={
+    method :"POST",
+    headers: 
+    {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+    }
 
+    body: JSON.stringify({
+    "name": "Jessie",
+    "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+    "likes": 0
+    })
+  };
+  
 form.addEventListener("submit" , ()=>{
-
+  
+  fetch("http://localhost:3000/toys" , option)
+  .then(response => response.json())
+  .then(json =>{});
 })
 
   fetch("http://localhost:3000/toys")
