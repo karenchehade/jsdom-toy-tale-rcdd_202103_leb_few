3d136fe70721b26bb3c095fd8bb01290e0f5bd23
karen chehade <karen_chehade@hotmail.com>
2021-05-03 18:33:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   const toy-collection = document.getElementById('toy-collection');
+  
   fetch("http://localhost:3000/toys")
   .then(response => response.json())
   .then(json =>{
