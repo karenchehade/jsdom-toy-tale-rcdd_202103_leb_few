6753965b357a92c23143e347c28b60d4f7df5147
karen chehade <karen_chehade@hotmail.com>
2021-05-03 18:29:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toy-collection = document.getElementById('toy-collection');
+  fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(json =>{
+    toy-collection.insertAdjacentHTML("beforeend","<div class='card'> </div>")
+  })
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
